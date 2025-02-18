b690585b7c4dd7023d5eaa269bbc251399cfe66f
Abdul Moiz Khan <moizkhanxx99@gmail.com>
2025-02-18 23:59:19 +0500

    completed project

--- 11-todo-list.js
+++ 11-todo-list.js
@@ -6,20 +6,34 @@ function rendorTodoList() {
   let todoListHTML = '';
 
   for (let i = 0; i < todoList.length; i++) {
-    const todo = todoList[i];
-    const html = `<p>${todo}</p>`;
+    const todoObject = todoList[i];
+    const { name, dueDate } = todoObject;
+
+    const html = `
+    <div>${name}</div>
+    <div>${dueDate}</div>
+    <button onclick="
+      todoList.splice(${i}, 1);
+      rendorTodoList();
+      " class="delete-button-css">delete</button>
+    `;
     todoListHTML += html;
   }
   document.querySelector('.js-show-result').innerHTML = todoListHTML;
-}
+} 
 
 
 function addTodo() {
   const inputElement = document.querySelector('.js-todo-name');
   const name = inputElement.value;
 
-  todoList.push(name);
-  console.log(todoList);
+  const dueDateELement = document.querySelector('.js-due-date');
+  const dueDate = dueDateELement.value;
+
+  todoList.push({
+    name,
+    dueDate
+  });
 
   inputElement.value = '';
 
